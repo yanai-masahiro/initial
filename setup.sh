b18d245163f39ce52cbb9ab82f22b5b5e2103846
yanap <yanap1214@gmail.com>
2016-07-21 16:15:03 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install zsh vim git tmux tree htop go nmap gibo neovim node sqlite tig highway
+brew install caskroom/cask/brew-cask
+brew tap peco/peco
+brew install peco
+brew tap motemen/ghq
+brew install ghq
