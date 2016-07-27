365a7e9ee4df1f96a6995c586501fe68e90d6e3c
yanap <yanap1214@gmail.com>
2016-07-27 18:18:56 +0900

    modify package:

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 
  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install zsh vim git tmux tree htop go nmap gibo node sqlite tig highway
+
+brew install zsh vim git tmux tree htop go nmap gibo node sqlite tig
 brew install caskroom/cask/brew-cask
 brew tap peco/peco
 brew install peco
 brew tap motemen/ghq
 brew install ghq
+brew tap tkengo/highway
+brew install highway
