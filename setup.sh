5fed1bcec1823329bddd68a7923df65ac94abc47
yanap <yanap1214@gmail.com>
2016-07-27 18:10:38 +0900

    modify setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install zsh vim git tmux tree htop go nmap gibo neovim node sqlite tig highway
+brew install zsh vim git tmux tree htop go nmap gibo node sqlite tig highway
 brew install caskroom/cask/brew-cask
 brew tap peco/peco
 brew install peco
