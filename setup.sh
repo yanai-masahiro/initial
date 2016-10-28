b3adb3dd4f09e9b530af14fd6c46470cb24c692f
S041897 <s041897@cmlabs.jp>
2016-10-28 13:51:43 +0900

    delete motemen/ghq

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ brew install zsh vim git tmux tree htop go nmap gibo node sqlite tig
 brew tap caskroom/cask
 brew tap peco/peco
 brew install peco
-brew tap motemen/ghq
 brew install ghq
 brew tap tkengo/highway
 brew install highway
