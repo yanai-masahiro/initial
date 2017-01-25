aeafe315014f5de0dbe32e1fa4086a94f3b66b1d
Masahiro Yanai <yanai@MasahironoMacBook-Pro.local>
2017-01-25 18:43:47 +0900

    add zsh-completions

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 
-brew install zsh vim git tmux tree htop go nmap gibo node sqlite tig
+brew install zsh zsh-completions
+brew install vim git tmux tree htop go nmap gibo node sqlite tig
 brew tap caskroom/cask
 brew tap peco/peco
 brew install peco
